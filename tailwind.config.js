cb68aa83edc71d509614d3c548c56bff863b51fd
AntonCanzobre <anton.c744@gmail.com>
2025-02-21 23:48:32 +0100

    Styles for home page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+      'azul-patrimonio': '#2C3E50',
+      'oro-clasico': '#D4AF37',
+      'marmol-beige': '#EAE0C8',
+      'granate-renacentista': '#8B0000',
+      'gris-pizarra': '#5A5A5A',
+    },
+  },
+  },
+  plugins: [],
+};
