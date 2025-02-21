30661e498b22ce77ad4358a12e70206804db91f0
mario-diez <mario.diez@udc.es>
2025-02-22 00:26:25 +0100

    camera componente initialized

--- src/app/components/camera/camera.component.ts
+++ src/app/components/camera/camera.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.component.html',
   styleUrls: ['./camera.component.scss'],
+  imports: [IonicModule,CommonModule]
 })
-export class CameraComponent  implements OnInit {
+export class CameraComponent{
+  imageUrl: string | null = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {}
+  async takePhoto() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Base64,
+        source: CameraSource.Camera
+      });
 
+      this.imageUrl = `data:image/jpeg;base64,${image.base64String}`;
+    } catch (error) {
+      console.error("Error when taking photo:", error);
+    }
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CameraComponent } from '../components/camera/camera.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,CameraComponent],
 })
 export class HomePage {
   constructor() {}
