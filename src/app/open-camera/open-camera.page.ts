dbf9ecf3985573899975d1a58378bd2e3426b220
mario-diez <mario.diez@udc.es>
2025-02-22 13:01:58 +0100

    texto to speech

--- src/app/open-camera/open-camera.page.ts
+++ src/app/open-camera/open-camera.page.ts
@@ -6,22 +6,37 @@ import { Observable } from 'rxjs';
 import { ProfileService } from '../store/profile/profile.service';
 import { CameraComponent } from '../components/camera/camera.component';
 import { ArtCardComponent } from '../components/art-card/art-card.component';
-
+import { TextToSpeech } from '@capacitor-community/text-to-speech';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-open-camera',
   templateUrl: './open-camera.page.html',
   styleUrls: ['./open-camera.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, CameraComponent, ArtCardComponent]
+  imports: [CommonModule, IonicModule,FormsModule, CameraComponent, ArtCardComponent]
 })
 export class OpenCameraPage {
 
   profile$: Observable<string | null>;
+  text = "";
 
   constructor(private profileService: ProfileService) {
     this.profile$ = this.profileService.getProfile();
     console.log(this.profile$)
+  }
+
+  async speak() {
+    if (this.text.trim() === '') return;
+
+    await TextToSpeech.speak({
+      text: this.text,
+      lang: 'es-ES',
+      rate: 0.8, 
+      pitch: 1.0,
+      volume: 1.0,
+      category: 'ambient', 
+    });
   }
 
 }
